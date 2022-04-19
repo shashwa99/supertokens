cdc53c49073df8bbefd96e71ad84710b1f3080c7
shashwat <shashwatnegi10@gmail.com>
2022-04-19 17:41:55 +0530

    updated frontend

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+import Logout from "./Logout";
+import { useSessionContext } from "supertokens-auth-react/recipe/session";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+
 export default function Home() {
- return (
-   <>
-     <h1>Hello from SuperTokens</h1>
-   </>
- );
+  const { userId } = useSessionContext();
+  const navigate = useNavigate();
+
+  async function logoutClicked() {
+    await signOut();
+    navigate("/auth");
+  }
+
+  return (
+    <div className="fill">
+      <Logout logoutClicked={logoutClicked} />
+      {userId}
+    </div>
+  );
 }
